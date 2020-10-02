3d3f52194535629a5fefffaa8aab110c8b032250
Cassio Seffrin <cassioseffrin@gmail.com>
2020-10-01 21:11:44 -0300

    Generics

--- src/aula6/ArrayGenerico.java
+++ src/aula6/ArrayGenerico.java
@@ -12,24 +12,19 @@ public class ArrayGenerico {
 	}
 	public static void imprimirArray(String[] elementos){
 		for (int i=0; i< elementos.length; i++){
-			System.out.printf("\n int %s : %s",i, elementos[i]);
+			System.out.printf("\n String %s : %s",i, elementos[i]);
 		}
 	}
 	
-	public static <E> void  imprimirArrayGeneric(E[] elementos){
+	public static void imprimirArray(Date[] elementos){
 		for (int i=0; i< elementos.length; i++){
-			System.out.printf("\n int %s : %s",i, elementos[i]);
+			System.out.printf("\n Date %s : %s",i, elementos[i]);
 		}
-		
 	}
 	
-	public static void main (String a[]){
-		Integer[] inteiros = {1,2,5,6,2,3,6};
-		String[] strings = {"teste1","teste2","teste3"};
-		Date[] datas = {new Date(), new Date(118,5,23)};
-		imprimirArrayGeneric(inteiros);	
-		imprimirArrayGeneric(strings);
-		imprimirArrayGeneric(datas);
+	public static <E> void imprimirArrayGeneric(E[] elementos){
+		for (int i=0; i < elementos.length; i++){
+			System.out.printf("\n Elemento <E>: %s : %s",i, elementos[i]);
+		}
 	}
-
 }
