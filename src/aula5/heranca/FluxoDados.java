d297c8281aa7b412ce78ddad2635bd548d4c3288
Cassio Seffrin <cassioseffrin@gmail.com>
2020-10-25 17:54:16 -0300

    serializacao

---
+++ src/aula5/heranca/FluxoDados.java
@@ -0,0 +1,17 @@
+package aula5.heranca;
+
+import java.util.ArrayList;
+
+public interface FluxoDados {
+
+	public boolean gravarArquivoCSV( )  ;
+	
+	public boolean gravarArquivoSerial();
+	
+	public ArrayList<Pessoa> lerTodos();
+	
+	public void imprimirColecaoPessoas(ArrayList<Pessoa> lst);
+
+	Pessoa lerArquivoSerial() throws ClassNotFoundException;
+
+}
